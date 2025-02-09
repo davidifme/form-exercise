6f520f379f7c5f7bb8a4428f9516442e0d2c9af5
David <davidifme@icloud.com>
2025-02-09 19:19:06 +0100

    Add form validation with error handling
    - Added `error-icon.png` for displaying error icons.
    - Implemented form validation logic in `src/form.js`
    to handle input validation and display error messages.
    - Updated `src/index.js` to import the new form validation script.
    - Modified `src/template.html` to include necessary form fields
    and error message placeholders.
    - Added `styles/main.css` to style the form and error messages.

---
+++ src/form.js
@@ -0,0 +1,76 @@
+import errorIcon from "/icons/error-icon.png";
+
+const form = document.querySelector('form');
+const password = document.getElementById('password');
+const passwordConfirmation = document.getElementById('password-confirmation');
+
+const inputs = document.querySelectorAll('input');
+
+passwordConfirmation.addEventListener('blur', () => {
+    if (passwordConfirmation.value !== password.value) {
+      passwordConfirmation.setCustomValidity("Passwords don't match");
+    } else {
+      passwordConfirmation.setCustomValidity("");
+    }
+  });
+
+inputs.forEach(input => {
+    input.addEventListener('blur', (event) => {
+        const error = document.querySelector(`.error[data-type="${input.name}"]`)
+        
+        if (input.value) {
+            if (input.validity.valid) {
+                error.textContent = '';
+                error.className = 'error';
+            } else {
+                showError(input);
+            }
+        } else {
+            error.textContent = '';
+            error.className = 'error';
+        }
+    });
+});
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const firstInvalidInput = Array.from(inputs).find(input => !input.validity.valid);
+    if (firstInvalidInput) {
+        showError(firstInvalidInput);
+        firstInvalidInput.focus();
+    } else {
+        formSubmit();
+        form.reset();
+    }
+});
+
+function showError(inputElement) {
+    const error = document.querySelector(`.error[data-type="${inputElement.name}"]`);
+
+    if (inputElement.validity.valueMissing) {
+        error.textContent = `This field is required`;
+    } else if (inputElement.validity.typeMismatch) {
+        error.textContent = `Enter a valid ${inputElement.name}`;
+    } else if (inputElement.validity.tooShort) {
+        error.textContent = `Please enter at least ${inputElement.minLength} characters`;
+    } else if (inputElement.validity.patternMismatch) {
+        error.textContent = `Invalid ${inputElement.name} format`;
+    } else if (inputElement.name === 'password-confirmation') {
+        error.textContent = inputElement.validationMessage;
+    }
+
+    appendErrorIcon(error);
+    error.className = 'error active';
+}
+
+function appendErrorIcon(domElement) {
+    const errorIconElement = document.createElement('img');
+    errorIconElement.src = errorIcon;
+    errorIconElement.alt = 'error-icon';
+    errorIconElement.classList.add('error-icon');
+    domElement.appendChild(errorIconElement);
+}
+
+function formSubmit() {
+    console.log('Form Submited');
+}
